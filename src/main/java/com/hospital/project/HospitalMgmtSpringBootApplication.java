87058ba18a40dd7c407c338bee4f4803a8e7da93
Ganesh Allampalli <ganeshallampalli@gmail.com>
2021-06-14 12:36:47 +0530

    :recycle: feat: renamed application boot

---
+++ src/main/java/com/hospital/project/HospitalMgmtSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableAutoConfiguration
+public class HospitalMgmtSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalMgmtSpringBootApplication.class, args);
+    }
+}
