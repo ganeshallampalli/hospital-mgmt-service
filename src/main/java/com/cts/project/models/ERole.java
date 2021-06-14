977ee95209fbbe88d820ca2b33d1657d4185399f
Ganesh Allampalli <ganeshallampalli@gmail.com>
2021-06-14 12:36:07 +0530

    :passport_control: feat: updated roles

--- src/main/java/com/cts/project/models/ERole.java
+++
@@ -1,8 +0,0 @@
-package com.cts.project.models;
-
-public enum ERole {
-    ROLE_DOCTOR,
-    ROLE_RECEPTION,
-    ROLE_PATIENT,
-    ROLE_ADMIN,
-}
---
+++ src/main/java/com/hospital/project/models/ERole.java
@@ -0,0 +1,7 @@
+package com.hospital.project.models;
+
+public enum ERole {
+    ROLE_TECHNICIAN,
+    ROLE_CUSTOMER,
+    ROLE_ADMIN,
+}
